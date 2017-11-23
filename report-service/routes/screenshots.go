b1f8e0d9195efbc911390488cabc33cfe3042574
Stefan Huber <hubidu@users.noreply.github.com>
2017-11-23 12:34:10 +0100

    Deliver placeholder images for missing screenshot files

--- report-service/routes/screenshots.go
+++ report-service/routes/screenshots.go
@@ -3,7 +3,9 @@ package routes
 import (
 	b64 "encoding/base64"
 	"fmt"
+	"net/http"
 	url "net/url"
+	"os"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
@@ -21,12 +23,12 @@ func GetScreenshotImg(c *gin.Context) {
 	filePlain, _ := url.PathUnescape(string(file))
 
 	screenshotPath := filepath.Join(baseDir, pathPlain, filePlain)
-	fmt.Println("Screenshot file path", screenshotPath)
 
-	// if _, err := os.Stat(screenshotPath); os.IsNotExist(err) {
-	// 	// return a placeholder image if the screenshot does not exist (because it has been cleaned up)
-	// 	c.Redirect(http.StatusMovedPermanently, "http://via.placeholder.com/800x600/")
-	// } else {
-	c.File(screenshotPath)
-	// }
+	if _, err := os.Stat(screenshotPath); os.IsNotExist(err) {
+		fmt.Println("Screenshot not found (already archived?)", screenshotPath)
+		// return a placeholder image if the screenshot does not exist (because it has been cleaned up)
+		c.Redirect(http.StatusMovedPermanently, "http://via.placeholder.com/800x600/")
+	} else {
+		c.File(screenshotPath)
+	}
 }
