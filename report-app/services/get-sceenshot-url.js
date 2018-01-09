e2975064a9db8619dec9071e0dbd8ef6b9dd9361
Stefan Huber <hubidu@users.noreply.github.com>
2018-01-09 17:14:18 +0100

    Fix screenshot url

--- report-app/services/get-sceenshot-url.js
+++ report-app/services/get-sceenshot-url.js
@@ -1,7 +1,5 @@
 const base64 = require('base-64')
 import config from './config'
 
-const ReportServiceUrl = `http://${config.ReportServiceHost}`
-
 // Notice the double encoding. It's because of gin-gonic
-export default (path, filename) => `${ReportServiceUrl}/screenshots/${base64.encode(encodeURIComponent(path))}/${base64.encode(encodeURIComponent(filename))}`
+export default (path, filename) => `http://${config.ReportServiceHost}/screenshots/${base64.encode(encodeURIComponent(path))}/${base64.encode(encodeURIComponent(filename))}`
