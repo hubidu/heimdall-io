27bde2109a177ab40378e5884434c110c6f32e66
Stefan Huber <hubidu@users.noreply.github.com>
2018-04-23 17:07:14 +0200

    Rename build.sh

---
+++ build-all.sh
@@ -0,0 +1,4 @@
+cd report-app && ./build.sh && cd .. || exit 1
+cd report-importer && ./build.sh && cd .. || exit 1
+cd report-service && ./build.sh && cd .. || exit 1
+cd alert-service && ./build.sh && cd .. || exit 1
