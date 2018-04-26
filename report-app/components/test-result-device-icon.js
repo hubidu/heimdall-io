1d2f642e664f8165960cc9aefcd81bec1a0592e6
Stefan Huber <hubidu@users.noreply.github.com>
2018-04-26 09:46:05 +0200

    Add browser icons

--- report-app/components/test-result-device-icon.js
+++ report-app/components/test-result-device-icon.js
@@ -3,12 +3,24 @@ import MobileIcon from 'react-icons/lib/fa/mobile'
 import TabletIcon from 'react-icons/lib/fa/tablet'
 import AndroidIcon from 'react-icons/lib/fa/android'
 import IOSIcon from 'react-icons/lib/fa/apple'
+import FirefoxIcon from 'react-icons/lib/fa/firefox'
+import ChromeIcon from 'react-icons/lib/fa/firefox'
+import InternetExplorerIcon from 'react-icons/lib/fa/internet-explorer'
 
 export default ({result, deviceSettings}) => {
   const color = (result === 'error' || result === false) ? 'orange' : 'green'
 
   return (
     <span className={`f4 mr1 ${color}`} title={deviceSettings ? `${deviceSettings.Type} ${deviceSettings.Width}x${deviceSettings.Height}` : ''} >
+      {
+        deviceSettings && deviceSettings.Browser === 'chrome' ? <ChromeIcon /> : null
+      }
+      {
+        deviceSettings && deviceSettings.Browser === 'firefox' ? <FirefoxIcon /> : null
+      }
+      {
+        deviceSettings && deviceSettings.Browser === 'ie' ? <InternetExplorerIcon /> : null
+      }
       {
         deviceSettings && deviceSettings.Type === 'desktop' ? <DesktopIcon /> : null
       }
