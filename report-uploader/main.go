534cbd5f74583469fe14113429553a7daf696fb8
Stefan Huber <hubidu@users.noreply.github.com>
2018-04-26 09:38:14 +0200

    WIP Add token create route

--- report-uploader/main.go
+++ report-uploader/main.go
@@ -1,75 +1,10 @@
 package main
 
 import (
-	"archive/zip"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path"
-	"path/filepath"
-
 	"github.com/gin-gonic/gin"
+	tokens "github.com/hubidu/e2e-backend/report-uploader/routes"
+	uploads "github.com/hubidu/e2e-backend/report-uploader/routes"
 )
-
-func unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	os.MkdirAll(dest, 0755)
-
-	// Closure to address file descriptors issue with all the deferred .Close() methods
-	extractAndWriteFile := func(f *zip.File) error {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		path := filepath.Join(dest, f.Name)
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, f.Mode())
-		} else {
-			os.MkdirAll(filepath.Dir(path), f.Mode())
-			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	for _, f := range r.File {
-		err := extractAndWriteFile(f)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
 
 func main() {
 	router := gin.Default()
@@ -77,29 +12,8 @@ func main() {
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	// router.MaxMultipartMemory = 8 << 20 // 8 MiB
 
-	UploadDir := "./uploads"
-	os.MkdirAll(UploadDir, os.ModePerm)
-	ReportDir := "./reports"
-	os.MkdirAll(ReportDir, os.ModePerm)
-
-	router.Static("/", UploadDir)
-	router.POST("/upload", func(c *gin.Context) {
-		file, err := c.FormFile("report_data")
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
-			return
-		}
-
-		zipFile := path.Join(UploadDir, file.Filename)
-		if err := c.SaveUploadedFile(file, zipFile); err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
-			return
-		}
-
-		unzip(zipFile, path.Join(ReportDir, "."))
-		os.Remove(zipFile)
-
-		c.String(http.StatusOK, fmt.Sprintf("Report successfully uploaded"))
-	})
+	// router.Static("/", UploadDir)
+	router.POST("/upload", uploads.ReceiveZippedReportData)
+	router.GET("/tokens/create/:user", tokens.CreateToken)
 	router.Run("0.0.0.0:8000")
 }
